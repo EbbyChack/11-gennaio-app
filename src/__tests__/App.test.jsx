65635a00b6fc7b88fb0ea1820122b6e62cf60cd6
Ebby Chakkungal <121340114+vlirkbog@users.noreply.github.com>
2024-01-11 16:21:56 +0100

    ok

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,9 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Welcome from "../components/Welcome";
+import CommentArea from "../components/CommentArea";
 
 it("Welcome has mounted", () => {
   render(<Welcome />);
 
   expect(screen.getByText("Benvenuti in EpiBooks!")).toBeInTheDocument();
+});
+
+it("CommentArea has mounted", () => {
+  render(<CommentArea />);
+
+  expect(screen.getByTestId("comment-area")).toBeInTheDocument();
 });
 
